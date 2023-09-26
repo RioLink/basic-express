8bb7d8966d739a6774a724ecd58a4aef2ab3a730
Alex <oleksandr.kudelya96@gmail.com>
2023-09-26 23:53:48 +0300

    basic-express

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello!");
+const express = require('express');
+const app = express();
+const path = require("path");
+
+app.get("/about", (req, res) => {
+    res.status(200).send("HELLO WORLD!");
+});
+app.get("/", (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, "./page1.html"))
+});
+app.get("/page2.html", (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, "./page2.html"))
+});
+
+app.all("*", (req, res) => {
+    res.status(404).send("<h1>Error! 404 Not found !</h1>");
+});
+
+app.listen(8000, () => {
+    console.log("Hello World!");
+}); 
